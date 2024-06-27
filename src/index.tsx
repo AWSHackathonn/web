430d1aafb1c0a31bd2442e66bc8dda61ad7d5694
xxxjinn <tlsguswls319@naver.com>
2024-06-27 15:49:07 +0900

    styles: globalstyle 적용

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import {theme} from './styles/Theme'
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
-  <BrowserRouter>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>,
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>,
 );
