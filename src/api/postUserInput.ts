50fb6f38f2223207bd48101b97f476c3dcc8d67b
xxxjinn <tlsguswls319@naver.com>
2024-06-28 10:33:39 +0900

    dev: post api 설정 / 일부 추가하기 컴포넌트 추가

---
+++ src/api/postUserInput.ts
@@ -0,0 +1,23 @@
+import { instance } from ".";
+
+interface Question {
+    question: string;
+    address: string[];
+    otherData: string;
+  }
+
+interface PostUserInputType{
+    jobRole: string;
+    company: string;
+    questionList: Question[];
+}
+
+export const postUserInput = async (body: PostUserInputType) => {
+    try {
+        const response = await instance.post('/profile', body);
+        return response.data;
+    } catch (error) {
+        console.error('Error posting user input:', error);
+        throw error;
+    }
+};
