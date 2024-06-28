f8eaf8b0c4edc86dc46065d893690cce99dccbfa
xxxjinn <tlsguswls319@naver.com>
2024-06-28 15:52:04 +0900

    f

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const instance = axios.create({
     baseURL:process.env.REACT_APP_SERVER_URL,
-    timeout:30000,
+    timeout:50000,
     headers:{
         'Content-Type':'application/json',
     }
